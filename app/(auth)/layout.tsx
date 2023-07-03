59342f844b8f773006b2eaa9b1d5b707a12ec913
JamesWoo <JamesWoo>
2023-07-03 20:35:32 +0900

    add modal

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,6 @@ export default function AUthLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex items-center justify-center h-full w-full">
-      {children}
-    </div>
+    <div className="flex items-center justify-center h-full">{children}</div>
   )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function SetupPage() {
-  return <div className="p-4">This is a protected route!</div>
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  )
 }
