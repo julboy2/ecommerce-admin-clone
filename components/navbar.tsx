8ef85f3358ab16af141f6e9351ceceef06d3fd47
JamesWoo <JamesWoo>
2023-07-12 22:41:02 +0900

    add Popover

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,27 @@
-'use client'
-
-import { UserButton } from '@clerk/nextjs'
+import { UserButton, auth } from '@clerk/nextjs'
 import { MainNav } from '@/components/main-nav'
+import StoreSwicher from '@/components/store-swicher'
+import { redirect } from 'next/navigation'
+import prismadb from '@/lib/prismadb'
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  })
+
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>This will be a store switcher</div>
-        <MainNav />
+        <StoreSwicher items={stores} />
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
         </div>
