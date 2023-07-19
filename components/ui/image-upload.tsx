6427ef0ea4d99af4047297b0c45aac37c23fdc75
JamesWoo <JamesWoo>
2023-07-19 20:44:25 +0900

    add 사진첨부

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,6 +1,10 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { ImagePlus, Trash } from 'lucide-react'
+import Image from 'next/image'
+import { CldUploadWidget } from 'next-cloudinary'
 
 interface ImageUploadProps {
   disabled?: boolean
@@ -29,5 +33,51 @@ export default function ImageUpload({
     return null
   }
 
-  return <div>ImageUpload</div>
+  return (
+    <div>
+      <div>
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+          >
+            <div className="z-10 absolute top-2 right-2">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                variant={'destructive'}
+                size={'icon'}
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image fill className="object-cover" alt="Image" src={url} />
+          </div>
+        ))}
+      </div>
+      {/* console.cloudinay 가서 Add upload preset클릭 하면 
+      Upload preset name 을 복사해서 uploadPreset 에 적을것  
+      Mode 가 Unsigned 로 되어 있어야 업로드됨
+      */}
+      <CldUploadWidget onUpload={onUpload} uploadPreset="qyusgxlp">
+        {({ open }) => {
+          const onClick = () => {
+            open()
+          }
+
+          return (
+            <Button
+              type="button"
+              disabled={disabled}
+              variant={'secondary'}
+              onClick={onClick}
+            >
+              <ImagePlus className="h-4 w-4 mr-2" />
+              Upload an Image
+            </Button>
+          )
+        }}
+      </CldUploadWidget>
+    </div>
+  )
 }
