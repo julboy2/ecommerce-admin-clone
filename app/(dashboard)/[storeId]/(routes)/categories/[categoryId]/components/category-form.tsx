8e3fd329afefd80ceb57bfc69399f4d5bd0a45a3
JamesWoo <JamesWoo>
2023-08-18 20:35:04 +0900

    카테고리 생성,수정,삭제 완성

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -67,15 +67,15 @@ export function CategoryForm({ initiaData, billboards }: CategoryFormProps) {
       setLoading(true)
       if (initiaData) {
         await axios.patch(
-          `/api/${params.storeId}/billboards/${params.billboardId}`,
+          `/api/${params.storeId}/categories/${params.categoryId}`,
           data
         )
       } else {
-        await axios.post(`/api/${params.storeId}/billboards`, data)
+        await axios.post(`/api/${params.storeId}/categories`, data)
       }
 
       router.refresh()
-      router.push(`/${params.storeId}/billboards`)
+      router.push(`/${params.storeId}/categories`)
       toast.success(toastMessage)
     } catch (error) {
       toast.error('Something went wrong')
@@ -88,14 +88,14 @@ export function CategoryForm({ initiaData, billboards }: CategoryFormProps) {
     try {
       setLoading(true)
       await axios.delete(
-        `/api/${params.storeId}/billboards/${params.billboardId}`
+        `/api/${params.storeId}/categories/${params.categoryId}`
       )
       router.refresh()
-      router.push('/')
-      toast.success('Billboard deleted.')
+      router.push(`/${params.storeId}/categories`)
+      toast.success('Category deleted.')
     } catch (error) {
       toast.error(
-        'Make sure you removed all categories using this billboard first.'
+        'Make sure you removed all products using this category first.'
       )
     } finally {
       setLoading(false)
--- app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
-import { BillboardColumn } from './columns'
+import { CategoryColumn } from './columns'
 import { Copy, Edit, MoreHorizontal, Trash } from 'lucide-react'
 import { toast } from 'react-hot-toast'
 import { useParams, useRouter } from 'next/navigation'
@@ -17,7 +17,7 @@ import { useState } from 'react'
 import { AlertModal } from '@/components/modals/alert-modal'
 
 interface CellActionProps {
-  data: BillboardColumn
+  data: CategoryColumn
 }
 
 export function CellAction({ data }: CellActionProps) {
@@ -29,17 +29,19 @@ export function CellAction({ data }: CellActionProps) {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id)
-    toast.success('Billboard Id copied to clipboard')
+    toast.success('Category Id copied to clipboard')
   }
 
   const onDelete = async () => {
     try {
       setLoading(true)
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`)
       router.refresh()
-      toast.success('Billboard deleted.')
+      toast.success('Category deleted.')
     } catch (error) {
-      toast.error('error')
+      toast.error(
+        'Make sure you removed all products using this category first.'
+      )
     } finally {
       setLoading(false)
       setOpen(false)
@@ -69,7 +71,7 @@ export function CellAction({ data }: CellActionProps) {
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/categories/${data.id}`)
             }
           >
             <Edit className="mr-2 h-4 w-4" />
